14266f40c41cb2473af00d8b3d991dbaf2aec241
HSU JUI SHENG <zed2786328@gmail.com>
2024-03-17 10:36:48 +0800

    hide real config

--- configs/env-config.js
+++ configs/env-config.js
@@ -1,5 +1,3 @@
-export const API_KEY =
-  "ULZfdHpKfVk5HwrlUNl55OT08JQpTiV15vXFGXnAgBD4Suy6ulkGc5qEd5c8KcXB";
-export const SECRET_KEY =
-  "YLmBBhlFiLmpyHPMDqMcTmrHjmtxakfnVuy6M8CzVknYEKe5wa4m5tLVwbWHHSrD";
+export const API_KEY = "YOUR_API_KEY";
+export const SECRET_KEY = "YOUR_SECRET_KEY";
 export const REST_BASEURL = "https://fapi.binance.com";
